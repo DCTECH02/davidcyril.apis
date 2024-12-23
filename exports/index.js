676296281c05669dd30c0ac7272951c252e5448f
DCTECH02 <davidcyrilx2005@gmail.com>
2024-12-23 17:42:24 +0100

    Add files via upload

--- exports/index.js
+++ exports/index.js
@@ -1,6 +1,6 @@
-import { Mp3, 
-         Mp4 } from './youtube.js';
+import { Mp3, Mp4 } from './youtube.js';
 import { chatbot } from './chatbot.js';
 import { tiktokdl } from './tiktok.js';
+import { Lyrics } from './lyrics.js'; // Import Lyrics module
 
-export { Mp3, Mp4, tiktokdl, chatbot };
+export { Mp3, Mp4, tiktokdl, chatbot, Lyrics }; // Export Lyrics alongside others
